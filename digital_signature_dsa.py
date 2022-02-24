aa45361c526f41eba5cf5f83b315d37045cc5db3
SushanthEnukonda <enukondasushanthreddy21@gmail.com>
2022-02-24 21:38:25 +0530

    updated codes

--- digital_signature_dsa.py
+++ digital_signature_dsa.py
@@ -4,11 +4,10 @@ from gmpy2 import xmpz, to_binary, invert, powmod, is_prime
 
 
 def generate_p_q(L, N):
-    g = N  # g >= 160
+    g = N
     n = (L - 1) // g
     b = (L - 1) % g
     while True:
-        # generate q
         while True:
             s = xmpz(randrange(1, 2 ** (g)))
             a = sha1(to_binary(s)).hexdigest()
@@ -19,9 +18,8 @@ def generate_p_q(L, N):
             q = U | mask
             if is_prime(q, 20):
                 break
-        # generate p
-        i = 0  # counter
-        j = 2  # offset
+        i = 0
+        j = 2
         while i < 4096:
             V = []
             for k in range(n + 1):
@@ -34,7 +32,7 @@ def generate_p_q(L, N):
             W += (V[n] % 2 ** b) * 2 ** (160 * n)
             X = W + 2 ** (L - 1)
             c = X % (2 * q)
-            p = X - c + 1  # p = X - (c - 1)
+            p = X - c + 1
             if p >= 2 ** (L - 1):
                 if is_prime(p, 10):
                     return p, q
@@ -53,7 +51,7 @@ def generate_g(p, q):
 
 
 def generate_keys(g, p, q):
-    x = randrange(2, q)  # x < q
+    x = randrange(2, q)
     y = powmod(g, x, p)
     return x, y
 
@@ -68,7 +66,7 @@ def sign(M, p, q, g, x):
     if not validate_params(p, q, g):
         raise Exception("Invalid params")
     while True:
-        k = randrange(2, q)  # k < q
+        k = randrange(2, q)
         r = powmod(g, k, p) % q
         m = int(sha1(M).hexdigest(), 16)
         try:
@@ -90,7 +88,6 @@ def verify(M, r, s, p, q, g, y):
     m = int(sha1(M).hexdigest(), 16)
     u1 = (m * w) % q
     u2 = (r * w) % q
-    # v = ((g ** u1 * y ** u2) % p) % q
     v = (powmod(g, u1, p) * powmod(y, u2, p)) % p % q
     if v == r:
         return True
